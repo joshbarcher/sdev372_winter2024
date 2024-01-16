0dcf8f4ba484fefee28b3dc26c1b4103beb230cf
Josh Archer <jarcher@greenriver.edu>
2024-01-16 13:33:06 -0800

    Refactored controller to a service layer.

--- movie-app/src/main/java/edu/greenriver/sdev/movieapp/controller/MovieApi.java
+++ movie-app/src/main/java/edu/greenriver/sdev/movieapp/controller/MovieApi.java
@@ -1,6 +1,7 @@
 package edu.greenriver.sdev.movieapp.controller;
 
 import edu.greenriver.sdev.movieapp.domain.Movie;
+import edu.greenriver.sdev.movieapp.service.MovieService;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -11,104 +12,49 @@ import java.util.Random;
 @RestController
 public class MovieApi
 {
-    private List<Movie> movies = new ArrayList<>(List.of(
-        new Movie("Inception", 2010, "Science Fiction", "PG-13", false),
-        new Movie("The Shawshank Redemption", 1994, "Drama", "R", false),
-        new Movie("The Dark Knight", 2008, "Action", "PG-13", false),
-        new Movie("Pulp Fiction", 1994, "Crime", "R", false),
-        new Movie("Titanic", 1997, "Drama", "PG-13", false),
-        new Movie("The Godfather", 1972, "Crime", "R", false),
-        new Movie("Avatar", 2009, "Action", "PG-13", true),
-        new Movie("The Lord of the Rings: The Return of the King", 2003, "Fantasy", "PG-13", false),
-        new Movie("Jurassic Park", 1993, "Science Fiction", "PG-13", false),
-        new Movie("Forrest Gump", 1994, "Drama", "PG-13", false)
-    ));
-
-    //REST apis are routes that pass data through HTTP
-    //(methods: GET, POST, PUT/PATCH, DELETE)
+    private MovieService service;
 
     //respond to (GET) requests at localhost:3000/movies/random
     @GetMapping("movies/random")
     public Movie getRandom()
     {
-        Random generator = new Random();
-        int index = generator.nextInt(movies.size());
-        return movies.get(index);
+        return service.getRandomMovie();
     }
 
     @GetMapping("movies/all")
     public List<Movie> all()
     {
-        return movies;
+        return service.all();
     }
 
     @GetMapping("movies/{title}")
     public Movie byTitle(@PathVariable String title)
     {
-        return getMovie(title);
-    }
-
-    private Movie getMovie(String title)
-    {
-        for (int i = 0; i < movies.size(); i++)
-        {
-            Movie next = movies.get(i);
-            if (next.getTitle().equalsIgnoreCase(title))
-            {
-                return next;
-            }
-        }
-
-        //we can use functional code (with lambdas)
-        /*Movie found = movies.stream()
-            .filter(movie -> movie.getTitle().equals(title))
-            .findFirst()
-            .orElse(null);*/
-        return null;
+        return service.byTitle(title);
     }
 
     @GetMapping("movies/year/{year}")
     public List<Movie> byYear(@PathVariable int year)
     {
-        List<Movie> results = new ArrayList<>();
-
-        for (int i = 0; i < movies.size(); i++)
-        {
-            Movie next = movies.get(i);
-            if (next.getYear() == year)
-            {
-                results.add(next);
-            }
-        }
-
-        return results;
+        return service.byYear(year);
     }
 
     @PostMapping("movies")
     public void addMovie(@RequestBody Movie newMovie)
     {
-        movies.add(newMovie);
+        service.addMovie(newMovie);
     }
 
     @PutMapping("movies/{title}")
     public Movie updateMovie(@PathVariable String title,
                             @RequestBody Movie updatedMovie)
     {
-        //find the movie that matches
-        Movie savedMovie = getMovie(title);
-
-        //update the data in the movie
-        savedMovie.setGenre(updatedMovie.getGenre());
-        savedMovie.setYear(updatedMovie.getYear());
-        savedMovie.setInternational(updatedMovie.isInternational());
-        savedMovie.setRating(updatedMovie.getRating());
-
-        return savedMovie;
+        return service.updateMovie(updatedMovie, title);
     }
 
-    @DeleteMapping("")
-    public void deleteMovie()
+    @DeleteMapping("movies/{title}")
+    public void deleteMovie(@PathVariable String title)
     {
-
+        service.deleteMovie(title);
     }
 }
---
+++ movie-app/src/main/java/edu/greenriver/sdev/movieapp/service/MovieService.java
@@ -0,0 +1,105 @@
+package edu.greenriver.sdev.movieapp.service;
+
+import edu.greenriver.sdev.movieapp.domain.Movie;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+
+/*
+    This class has business logic and is marked
+    as a stereotypical service layer.
+ */
+@Service
+public class MovieService
+{
+    private List<Movie> movies = new ArrayList<>(List.of(
+            new Movie("Inception", 2010, "Science Fiction", "PG-13", false),
+            new Movie("The Shawshank Redemption", 1994, "Drama", "R", false),
+            new Movie("The Dark Knight", 2008, "Action", "PG-13", false),
+            new Movie("Pulp Fiction", 1994, "Crime", "R", false),
+            new Movie("Titanic", 1997, "Drama", "PG-13", false),
+            new Movie("The Godfather", 1972, "Crime", "R", false),
+            new Movie("Avatar", 2009, "Action", "PG-13", true),
+            new Movie("The Lord of the Rings: The Return of the King", 2003, "Fantasy", "PG-13", false),
+            new Movie("Jurassic Park", 1993, "Science Fiction", "PG-13", false),
+            new Movie("Forrest Gump", 1994, "Drama", "PG-13", false)
+    ));
+
+    public Movie getRandomMovie()
+    {
+        Random generator = new Random();
+        int index = generator.nextInt(movies.size());
+        return movies.get(index);
+    }
+
+    public List<Movie> all()
+    {
+        //convert to read-only list
+        return Collections.unmodifiableList(movies);
+    }
+
+    public Movie byTitle(String title)
+    {
+        int index = movieIndexOf(title);
+        return movies.get(index);
+    }
+
+    public List<Movie> byYear(int year)
+    {
+        List<Movie> results = new ArrayList<>();
+
+        for (int i = 0; i < movies.size(); i++)
+        {
+            Movie next = movies.get(i);
+            if (next.getYear() == year)
+            {
+                results.add(next);
+            }
+        }
+
+        return results;
+    }
+
+    public void addMovie(Movie movie)
+    {
+        movies.add(movie);
+    }
+
+    public Movie updateMovie(Movie updatedMovie, String title)
+    {
+        //find the movie that matches
+        Movie savedMovie = movies.get(movieIndexOf(title));
+
+        //update the data in the movie
+        savedMovie.setGenre(updatedMovie.getGenre());
+        savedMovie.setYear(updatedMovie.getYear());
+        savedMovie.setInternational(updatedMovie.isInternational());
+        savedMovie.setRating(updatedMovie.getRating());
+
+        return savedMovie;
+    }
+
+    public void deleteMovie(String title)
+    {
+        int index = movieIndexOf(title);
+        movies.remove(index);
+    }
+
+    //returns the index where the matching movie title is found
+    private int movieIndexOf(String title)
+    {
+        for (int i = 0; i < movies.size(); i++)
+        {
+            Movie next = movies.get(i);
+            if (next.getTitle().equalsIgnoreCase(title))
+            {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+}
