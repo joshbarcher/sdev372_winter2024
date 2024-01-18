1f522b941efc2765c0c4c608b7f5e3f102f67eed
Josh Archer <jarcher@greenriver.edu>
2024-01-18 13:51:48 -0800

    Finished our data layer.

--- movie-app/src/main/java/edu/greenriver/sdev/movieapp/service/MovieService.java
+++ movie-app/src/main/java/edu/greenriver/sdev/movieapp/service/MovieService.java
@@ -1,5 +1,6 @@
 package edu.greenriver.sdev.movieapp.service;
 
+import edu.greenriver.sdev.movieapp.db.MovieRepository;
 import edu.greenriver.sdev.movieapp.domain.Movie;
 import org.springframework.stereotype.Service;
 
@@ -15,22 +16,17 @@ import java.util.Random;
 @Service
 public class MovieService
 {
-    private List<Movie> movies = new ArrayList<>(List.of(
-            new Movie(0, "Inception", 2010, "Science Fiction", "PG-13", false),
-            new Movie(0, "The Shawshank Redemption", 1994, "Drama", "R", false),
-            new Movie(0, "The Dark Knight", 2008, "Action", "PG-13", false),
-            new Movie(0, "Pulp Fiction", 1994, "Crime", "R", false),
-            new Movie(0, "Titanic", 1997, "Drama", "PG-13", false),
-            new Movie(0, "The Godfather", 1972, "Crime", "R", false),
-            new Movie(0, "Avatar", 2009, "Action", "PG-13", true),
-            new Movie(0, "The Lord of the Rings: The Return of the King", 2003, "Fantasy", "PG-13", false),
-            new Movie(0, "Jurassic Park", 1993, "Science Fiction", "PG-13", false),
-            new Movie(0, "Forrest Gump", 1994, "Drama", "PG-13", false)
-    ));
+    private MovieRepository repository;
+
+    public MovieService(MovieRepository repository)
+    {
+        this.repository = repository;
+    }
 
     public Movie getRandomMovie()
     {
         Random generator = new Random();
+        List<Movie> movies = repository.findAll();
         int index = generator.nextInt(movies.size());
         return movies.get(index);
     }
@@ -38,11 +34,13 @@ public class MovieService
     public List<Movie> all()
     {
         //convert to read-only list
+        List<Movie> movies = repository.findAll();
         return Collections.unmodifiableList(movies);
     }
 
     public Movie byTitle(String title)
     {
+        List<Movie> movies = repository.findAll();
         int index = movieIndexOf(title);
         return movies.get(index);
     }
@@ -50,6 +48,7 @@ public class MovieService
     public List<Movie> byYear(int year)
     {
         List<Movie> results = new ArrayList<>();
+        List<Movie> movies = repository.findAll();
 
         for (int i = 0; i < movies.size(); i++)
         {
@@ -65,13 +64,14 @@ public class MovieService
 
     public void addMovie(Movie movie)
     {
-        movies.add(movie);
+        //this will result in an INSERT SQL statement
+        repository.save(movie);
     }
 
     public Movie updateMovie(Movie updatedMovie, String title)
     {
         //find the movie that matches
-        Movie savedMovie = movies.get(movieIndexOf(title));
+        /*Movie savedMovie = movies.get(movieIndexOf(title));
 
         //update the data in the movie
         savedMovie.setGenre(updatedMovie.getGenre());
@@ -79,18 +79,20 @@ public class MovieService
         savedMovie.setInternational(updatedMovie.isInternational());
         savedMovie.setRating(updatedMovie.getRating());
 
-        return savedMovie;
+        return savedMovie;*/
+        return null;
     }
 
     public void deleteMovie(String title)
     {
-        int index = movieIndexOf(title);
-        movies.remove(index);
+        /*int index = movieIndexOf(title);
+        movies.remove(index);*/
     }
 
     //returns the index where the matching movie title is found
     private int movieIndexOf(String title)
     {
+        List<Movie> movies = repository.findAll();
         for (int i = 0; i < movies.size(); i++)
         {
             Movie next = movies.get(i);
