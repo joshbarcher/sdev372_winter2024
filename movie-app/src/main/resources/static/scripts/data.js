43e6ae84aae86085d94f4cabc51ed60f7b1bdcb2
Josh Archer <jarcher@greenriver.edu>
2024-02-06 13:57:23 -0800

    Added a second fetch() call using POST.

--- movie-app/src/main/resources/static/scripts/data.js
+++ movie-app/src/main/resources/static/scripts/data.js
@@ -1,22 +1,44 @@
 //wait until the page loads
-window.onload = function() {
+window.onload = async function() {
     //fetch the data
     const uri = "http://localhost:3000/api/v1/movies/all";
     const config = {
         method: 'get'
     }
-    fetch(uri, config)
-        .then(function(response) {
-            console.log(response);
 
-            //returns another promise
-            return response.json();
-        })
-        .then(function(data) {
-            //update our UI
-            console.log(data);
-            showMovies(data);
-        });
+    const response = await fetch(uri, config);
+    const data = await response.json();
+    showMovies(data);
+
+    //select the form button and handle form submission
+    const button = document.querySelector("button");
+    button.onclick = addMovie;
+}
+
+async function addMovie(event)
+{
+    //stop the form from submitting
+    event.preventDefault();
+
+    const newMovie = {
+        title: document.querySelector("#title").value,
+        genre: document.querySelector("#genre").value
+    }
+
+    const uri = "http://localhost:3000/api/v1/movies";
+    const config = {
+        method: "post",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(newMovie)
+    }
+
+    const response = await fetch(uri, config);
+    const movie = await response.json();
+
+    const section = document.querySelector("#movies");
+    addMovieSection(section, movie);
 }
 
 function showMovies(movies)
@@ -28,11 +50,15 @@ function showMovies(movies)
     for (let i = 0; i < movies.length; i++)
     {
         const movie = movies[i];
-
-        section.innerHTML += `<div class="movie">
-                <h2>${movie.title}</h2>
-                <p>ID #${movie.id}</p>
-                <p>Genre: ${movie.genre}</p>
-            </div>`;
+        addMovieSection(section, movie);
     }
+}
+
+function addMovieSection(section, movie)
+{
+    section.innerHTML += `<div class="movie">
+            <h2>${movie.title}</h2>
+            <p>ID #${movie.id}</p>
+            <p>Genre: ${movie.genre}</p>
+        </div>`;
 }
