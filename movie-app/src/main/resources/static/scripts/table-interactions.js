fcd165290d543501fbc86f3cb3b39b4a3f347339
Josh Archer <jarcher@greenriver.edu>
2024-02-29 12:55:41 -0800

    Add an example of DOM traversal.

--- movie-app/src/main/resources/static/scripts/table-interactions.js
+++ movie-app/src/main/resources/static/scripts/table-interactions.js
@@ -5,8 +5,45 @@ window.onload = function() {
     for (const link of editLinks) {
         link.onclick = editRecord;
     }
+
+    const deleteLinks = document.querySelectorAll('.delete');
+    for (const link of deleteLinks) {
+        link.onclick = deleteRecord;
+    }
+}
+
+function deleteRecord(evt) {
+    const deleteLink = evt.target;
+    const row = deleteLink.parentElement.parentElement;
+    const cells = row.children;
+
+    const id = cells[0].innerHTML;
+
+    //send the fetch request
+
+    //how do we remove the row?
+    row.remove();
 }
 
 function editRecord(evt) {
     console.log(evt.target);
+
+    //look for the id we are editing
+    const editLink = evt.target;
+    const row = editLink.parentElement.parentElement;
+    const cells = row.children;
+
+    const id = cells[0].innerHTML;
+    console.log(`Editing id ${id}`);
+
+    const species = cells[2].innerHTML;
+    console.log(`Editing species ${species}`);
+
+    const location = cells[4].innerHTML;
+
+    //replace the text with an input element
+    cells[2].innerHTML = `<input type="text" id="species" value="${species}">`;
+    cells[4].innerHTML = `<input type="text" id="location" value="${location}">`;
 }
+
+
